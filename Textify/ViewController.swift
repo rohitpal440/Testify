cb330b75e3cb5d5fc3970ffb4e3c67d793df413f
Rohit Pal <rohitpal440@gmail.com>
2016-08-12 15:17:17 +0530

    Adding project to github

--- Textify/ViewController.swift
+++ Textify/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
+
+    @IBOutlet weak var largeLabel: UILabel!
+    @IBOutlet weak var mediumLabel: UILabel!
+    @IBOutlet weak var smallLabel: UILabel!
+    @IBOutlet weak var text: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
 
+    func textFieldDidChange(textField: UITextField) {
+        //your code
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func textDidChanged(sender: UITextField) {
+        largeLabel.text = text.text
+        mediumLabel.text = text.text
+        smallLabel.text = text.text
+        
+    }
 }
 
