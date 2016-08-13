cf93119f070aea71584bac721f9fd64be1e15146
Rohit Pal <rohitpal440@gmail.com>
2016-08-13 16:56:44 +0530

    trying to focus on textfield  when keyboard appear

--- Textify/ViewController.swift
+++ Textify/ViewController.swift
@@ -14,28 +14,63 @@ class ViewController: UIViewController{
     @IBOutlet weak var mediumLabel: UILabel!
     @IBOutlet weak var smallLabel: UILabel!
     @IBOutlet weak var text: UITextField!
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        // Do any additional setup after loading the view, typically from a nib.
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillShow), name: UIKeyboardWillShowNotification, object: nil)
+//        
+//        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.keyboardWillHide), name: UIKeyboardWillHideNotification, object: nil)
+        
     }
-    
-
-    func textFieldDidChange(textField: UITextField) {
-        //your code
-    }
-    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+//    func keyboardWillShow(notification:NSNotification) {
+//        adjustingHeight(true, notification: notification)
+//    }
+//    
+//    func keyboardWillHide(notification:NSNotification) {
+//        adjustingHeight(false, notification: notification)
+//    }
+//    
+//    func adjustingHeight(show:Bool, notification:NSNotification) {
+//        var userInfo = notification.userInfo!
+//        let keyboardFrame:CGRect = (userInfo[UIKeyboardFrameBeginUserInfoKey] as! NSValue).CGRectValue()
+//        let animationDurarion = userInfo[UIKeyboardAnimationDurationUserInfoKey] as! NSTimeInterval
+//        let changeInHeight = (CGRectGetHeight(keyboardFrame) + 40) * (show ? 1 : -1)
+//        UIView.animateWithDuration(animationDurarion, animations: { () -> Void in
+//            if (self.bottomConstraint.constant) < changeInHeight && show {
+//                self.bottomConstraintOriginalValue = self.bottomConstraint.constant
+//                self.bottomConstraint.constant += (changeInHeight - self.bottomConstraint.constant)
+//            } else {
+//                
+//            }
+//            
+//        })
+//        
+//    }
+
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldDidChange(textField: UITextField) {
+        //code
+    }
 
     @IBAction func textDidChanged(sender: UITextField) {
-        largeLabel.text = text.text
-        mediumLabel.text = text.text
-        smallLabel.text = text.text
-        
+            self.largeLabel.text = self.text.text
+            self.mediumLabel.text = self.text.text
+            self.smallLabel.text = self.text.text
     }
 }
-
